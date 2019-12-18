0bc3f385db66e1c6c3fdec090f86379f2880be47
tom <tom.delacote@orange.fr>
2019-12-18 17:10:26 +0100

    Added draggable servers

--- main.cpp
+++ main.cpp
@@ -4,22 +4,30 @@
 #include "glutWindow.h"
 #include "src/Vector2.h"
 #include "src/Drone.h"
+#include "src/Server.h"
 
 class MainWindow: public GlutWindow {
 public:
 
     vector<Drone*> drones;
+    vector<Server*> servers;
 
-    GLuint droneId = 0;
+    GLuint droneId  = 0;
+    GLuint serverId = 0;
 
     MainWindow(const string &title, int argc, char **argv):
             GlutWindow(argc, argv, title,1000,500,FIXED) {
     };
 
+    bool MB_DOWN = false;
+    Server* selectedServer = nullptr;
+
     void onStart() override;
     void onDraw() override;
     void onQuit() override;
     void onMouseDown(int button, double x, double y) override;
+    void onMouseUp(int button, double x, double y) override;
+    void onMouseDrag(double x, double y) override;
     void onMouseMove(double x, double y) override;
     void onUpdate(double dt) override;
 
@@ -46,11 +54,16 @@ public:
 
 void MainWindow::onStart() {
 
-    // Load the drone texture
-    droneId = GlutWindow::loadTGATexture("../assets/drone.tga", 50, 50);
+    // Load the assets
+    droneId  = GlutWindow::loadTGATexture("../assets/drone.tga", 50, 50);
+    serverId = GlutWindow::loadTGATexture("../assets/antenna.tga", 64, 64);
 
     // Creates a new drone
     drones.push_back(new Drone(Vector2(10, 10), droneId));
+
+    // Creates servers
+    servers.push_back(new Server(Vector2(250, 250), "Demo 1", serverId));
+    servers.push_back(new Server(Vector2(650, 250), "Demo 2", serverId));
 
     glClearColor(1.0,1.0,1.0,1.0);
     glEnable(GL_BLEND);
@@ -58,8 +71,28 @@ void MainWindow::onStart() {
 }
 
 void MainWindow::onDraw() {
+
+    if (selectedServer) {
+        glColor3f(.75, .75,.95);
+        glPushMatrix();
+        fillEllipse(
+                selectedServer->position.x,
+                selectedServer->position.y - 10,
+                selectedServer->size + 10,
+                selectedServer->size + 10,
+                60
+        );
+        glPopMatrix();
+    }
+
     glEnable(GL_TEXTURE_2D);
     glColor3f(1.0f,1.0f,1.0f);
+
+    for (Server* server : servers) {
+        glPushMatrix();
+        server->onDraw();
+        glPopMatrix();
+    }
 
     for (Drone* drone : drones) {
         if (drone) {
@@ -115,9 +148,32 @@ void MainWindow::onMouseDown(int button, double x, double y) {
     }
 }
 
+void MainWindow::onMouseUp(int button, double x, double y) {
+    selectedServer = nullptr;
+}
+
 void MainWindow::onMouseMove(double x, double y) {
     for (Drone* drone : drones) {
         drone->target = Vector2(x, y);
+    }
+    if (MB_DOWN && selectedServer) {
+        selectedServer->position = Vector2(x, y);
+    }
+}
+
+void MainWindow::onMouseDrag(double x, double y) {
+    onMouseMove(x, y);
+    Vector2 mousePos = Vector2(x, y);
+    if (!selectedServer) {
+        for (Server* server : servers) {
+            if (mousePos.magnitude(server->position) < (server->size + 10)) {
+                selectedServer = server;
+                break;
+            }
+        }
+    }
+    if (selectedServer) {
+        selectedServer->position = mousePos;
     }
 }
 
---
+++ src/Server.h
@@ -0,0 +1,31 @@
+//
+// Created by tomde on 18/12/2019.
+//
+#ifndef VITV_SND_SERVER_H
+#define VITV_SND_SERVER_H
+
+#include "../glutWindow.h"
+#include "Vector2.h"
+
+class Server {
+public:
+
+    GLuint id; // TODO: Should be static
+
+    float size = 30;
+
+    Vector2 position;
+    string name;
+
+    /*
+     * Creates a server at pos
+     */
+    Server(Vector2 pos, string label, GLuint textureId);
+
+    /*
+     * Called at each frame to render the server
+     */
+    void onDraw();
+};
+
+#endif //VITV_SND_SERVER_H
