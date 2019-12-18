6f650643ff8334f238a81aba2cb33bd4d66a44f2
tom <tom.delacote@orange.fr>
2019-12-18 14:37:37 +0100

    Removing CMakeLists

--- main.cpp
+++ main.cpp
@@ -1,6 +1,54 @@
-#include <iostream>
+#pragma once
+#include "glutWindow.h"
+#include <vector>
 
-int main() {
-    std::cout << "Bonjour" << std::endl;
+class MainWindow: public GlutWindow {
+public:
+
+    MainWindow(const string &title,int argc,char **argv):
+            GlutWindow(argc,argv,title,1000,500,FIXED) {
+    };
+
+    void onStart() override;
+    void onDraw() override;
+    void onQuit() override;
+    void onMouseDown(int button, double x, double y) override;
+    void onMouseMove(double x, double y) override;
+    void onUpdate(double dt) override;
+};
+
+void MainWindow::onStart() {
+    glClearColor(1.0,1.0,1.0,1.0);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void MainWindow::onUpdate(double dt) {}
+
+void MainWindow::onDraw() {
+    glEnable(GL_TEXTURE_2D);
+    glColor3f(1.0f,1.0f,1.0f);
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
+
+#define MB_LEFTCLICK 0
+#define MB_RIGHTCLICK 2
+void MainWindow::onMouseDown(int button, double x, double y) {
+    if (button == MB_LEFTCLICK) {
+
+    }
+    if (button == MB_RIGHTCLICK) {
+
+    }
+}
+
+void MainWindow::onMouseMove(double x, double y) {}
+
+void MainWindow::onQuit() {}
+
+int main(int argc,char **argv) {
+    MainWindow win("Drones", argc, argv);
+    win.start();
     return 0;
 }
