659bdb972bec98131b2677ef2f6fb57af9628b2f
Starburste <vincentramousset@gmail.com>
2020-01-20 14:50:40 +0100

    Add the -h option message to show when -h is used in the options of the execution

--- main.cpp
+++ main.cpp
@@ -38,10 +38,12 @@ bool arg_is_option(char** begin, char** end, const std::string& option) {
  * @param argv : arguments
  * @return 0 for good exec ; 1 for the -h ; an another number otherwise
  */
-//TODO : Write the -h option
+//TODO : Write the -c option
 int main(int argc, char **argv) {
     if(argc == 2 && arg_is_option(argv, argv+argc, "-h")) {
-        std::cout << "" << std::endl;
+        std::cout << "Two options are available for this program : " << std::endl;
+        std::cout << "  -h : Give you the help for the different options ;" << std::endl;
+        std::cout << "  -c name_file : Use a configuration file different thant the original one." << std::endl;
         return 1;
     }
 
